d70a99c5a0e416cebfad020abfc6a86072881381
dridi.imen2014 Dridi <dridi.imen2014@gmail.com>
2018-10-04 10:03:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // write your code below!
+
+var name = "you"
+var holiday = "Mother day"
+var days = "20"
+
+
+function happyHolidays(){
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name){
+  console.log ('Happy holidays', name '!')
+}
+
+function happyHolidayTo(){
+return '"Happy", ${holiday}, ${name}!'
+}
+
+function holidayCountdown(){
+return '"It is", ${days} "days until", ${holiday}!'
+}
